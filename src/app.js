52a4f32a2c9ddb0af3f84db0deb1ba87dd0c0f22
sihyun <gthpgth@gmail.com>
2016-07-16 03:10:48 +0900

    dd

--- src/app.js
+++ src/app.js
@@ -6,17 +6,33 @@ function randomRange(n1, n2) {
     return Math.floor((Math.random() * (parseInt(n2) - parseInt(n1) + 1)) + parseInt(n1));
 };
 
+function lineRectIntersect(p1, p2, rect) {
+    var rectLine = [
+        [cc.p(rect.x, rect.y), cc.p(rect.x + rect.width, rect.y)],
+        [cc.p(rect.x + rect.width, rect.y), cc.p(rect.x + rect.width, rect.y + rect.height)],
+        [cc.p(rect.x + rect.width, rect.y + rect.height), cc.p(rect.x, rect.y + rect.height)],
+        [cc.p(rect.x, rect.y + rect.height), cc.p(rect.x, rect.y)],
+    ];
+
+    for (var i = 0; i < 4; ++i)
+        if (cc.pSegmentIntersect(p1, p2, rectLine[i][0], rectLine[i][1]))
+            return true;
+
+    return false;
+}
+
 var HelloWorldLayer = cc.Layer.extend({
     _draw: null,
-    _streak: null,
+    _fingerstreak: null,
     _thunderman: null,
     _moveList: [],
     _sprites: [],
+    _lineIdx: 0,
+
     ctor: function() {
         //////////////////////////////
         // 1. super init first
         this._super();
-        console.log(cc.pSegmentIntersect);
 
         /////////////////////////////
         // 2. add a menu item with "X" image, which is clicked to quit the program
@@ -59,11 +75,79 @@ var HelloWorldLayer = cc.Layer.extend({
         var r = 32;
         this._draw.drawCircle(cc.p(0, 0), r, 0, 20, false, 6, cc.color(0, 255, 0, 255));
 
-        this._streak = new cc.MotionStreak(3, 3, 6, cc.color.GREEN, res.streak);
-        this._streak.setPosition(this._thunderman.x + r / 2, this._thunderman.y + r / 2);
-        this.addChild(this._streak, 3);
+        this._fingerstreak = new cc.MotionStreak(3, 3, 6, cc.color.GREEN, res.streak);
+        this._fingerstreak.setPosition(0 + r / 2, 0 + r / 2);
+        //        this._fingerstreak.setPosition(this._thunderman.x + r / 2, this._thunderman.y + r / 2);
+        this.addChild(this._fingerstreak, 3);
 
         this.schedule(this.onUpdate);
+
+        return true;
+    },
+
+    onThunderManMoved: function() {
+        if (this._lineIdx >= this._moveList.length-1) {
+            this._thunderman.stopAllActions();
+            return;
+        }
+
+        var prevLineIdx = this._lineIdx;
+        this._lineIdx = Math.min(this._lineIdx + 3, this._moveList.length - 1);
+
+        var prevP = cc.p(this._thunderman.x, this._thunderman.y);
+        var p = cc.p(this._moveList[this._lineIdx][0], this._moveList[this._lineIdx][1]);
+        var distance = cc.pDistanceSQ(prevP, p);
+        var duration = 1 / 100000 * distance;
+        var move = cc.moveTo(duration, p);
+        //        console.log([this._lineIdx, this._moveList.length, distance, duration]);
+        var callback = cc.CallFunc.create(this.onThunderManMoved, this);
+        var seq = cc.Sequence.create([move, callback]);
+        this._thunderman.runAction(seq);
+
+        for (var i = prevLineIdx + 1; i <= this._lineIdx; ++i) {
+            var curP = cc.p(this._moveList[i][0], this._moveList[i][1]);
+            if((curP.x == prevP.x) || (curP.y == prevP.y))
+            {
+                console.log('wtf');
+            } 
+
+            for (var j in this._sprites) {
+                var redman = this._sprites[j];
+
+                if (lineRectIntersect(prevP, curP, redman.getBoundingBox())) {
+                    var blink = cc.Blink.create(1, 20);
+                    var callback = cc.CallFunc.create(function(data) {
+                        data.stopAllActions();
+                        data.setVisible(true);
+                    }, this, redman);
+                    var seq = cc.Sequence.create([blink, callback]);
+                    redman.runAction(seq);
+                }
+            }
+
+            prevP = curP;
+        }
+    },
+
+    onUpdate: function(delta) {
+        var pos = this._draw.convertToWorldSpace(cc.p(0, 0));
+        this._fingerstreak.x = pos.x + 32 / 2 - 6 * 2;
+        this._fingerstreak.y = pos.y + 32 / 2 - 6 * 2;
+        //this._streak.setPosition(this._thunderman.x + TILE_SIZE / 2, this._thunderman.y + TILE_SIZE / 2); 
+    },
+
+    containMap : function(ccp)
+    {
+        return cc.rectContainsPoint(cc.rect(0, 0, MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE), ccp); 
+    }
+
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
 
         cc.eventManager.addListener({
             event: cc.EventListener.TOUCH_ALL_AT_ONCE,
@@ -74,7 +158,22 @@ var HelloWorldLayer = cc.Layer.extend({
                 var touch = touches[0];
                 var touchLocation = touch.getLocation();
                 var layer = event.getCurrentTarget();
+
+                if(!layer.containMap(touchLocation))
+                    return;
+
+
                 var draw = layer._draw;
+
+                if (layer._moveList.length >= 1) {
+                    var lastPoint = layer._moveList[layer._moveList.length - 1];
+                    var prevP = cc.p(lastPoint[0], lastPoint[1]);
+                    var p = cc.p(touchLocation.x, touchLocation.y);
+                    var distance = cc.pDistanceSQ(prevP, p);
+                    if (distance < 100)
+                        return;
+                }
+
                 draw.x = touchLocation.x;
                 draw.y = touchLocation.y;
 
@@ -89,12 +188,16 @@ var HelloWorldLayer = cc.Layer.extend({
                 var touchLocation = touch.getLocation();
                 var draw = event.getCurrentTarget()._draw;
                 var layer = event.getCurrentTarget();
+
+                if(!layer.containMap(touchLocation))
+                    return;
+
                 draw.setVisible(true);
                 draw.x = touchLocation.x;
                 draw.y = touchLocation.y;
 
-                layer._streak.x = draw.x + 32 / 2 - 6 * 2;
-                layer._streak.y = draw.y + 32 / 2 - 6 * 2;
+                layer._fingerstreak.x = draw.x + 32 / 2 - 6 * 2;
+                layer._fingerstreak.y = draw.y + 32 / 2 - 6 * 2;
                 layer._moveList = [];
             },
 
@@ -106,32 +209,47 @@ var HelloWorldLayer = cc.Layer.extend({
                 var touchLocation = touch.getLocation();
                 var draw = event.getCurrentTarget()._draw;
                 var layer = event.getCurrentTarget();
+
+                if(!layer.containMap(touchLocation))
+                    return;
+
                 draw.setVisible(false);
                 draw.x = touchLocation.x;
                 draw.y = touchLocation.y;
 
-                if (layer._moveList.length <= 0)
+                if (layer._moveList.length <= 1)
                     return;
 
+                layer._lineIdx = 0;
+                layer._thunderman.x = layer._moveList[0][0];
+                layer._thunderman.y = layer._moveList[0][1];
+                var prevP = cc.p(layer._thunderman.x, layer._thunderman.y);
+                var p = cc.p(layer._moveList[1][0], layer._moveList[1][1]);
+                var distance = cc.pDistanceSQ(prevP, p);
+                var move = cc.moveTo(distance / (60 * 1000), p);
+                var callback = cc.CallFunc.create(layer.onThunderManMoved, layer);
+                var seq = cc.Sequence.create([move, callback]);
+                layer._thunderman.runAction(seq);
 
-                var list = [];
-                var prevX = layer._thunderman.x = layer._moveList[0][0];
-                var prevY = layer._thunderman.y = layer._moveList[0][1];
-                var prevCC = cc.p(prevX, prevY);
-                for (var i in layer._moveList) {
-                    var pos = layer._moveList[i];
-                    var p = cc.p(pos[0], pos[1]);
-                    var distance = cc.pDistanceSQ(prevCC, p);
-                    var move = cc.moveTo(distance / (60 * 1000), p);
-                    list.push(move);
-                    prevCC = p;
-                }
 
-                layer._thunderman.stopAllActions();
-                var action = cc.Sequence.create(list);
-
-                layer._thunderman.runAction(action);
-                layer._moveList = [];
+                //                var list = [];
+                //                var prevX = layer._thunderman.x = layer._moveList[0][0];
+                //                var prevY = layer._thunderman.y = layer._moveList[0][1];
+                //                var prevCC = cc.p(prevX, prevY);
+                //                for (var i in layer._moveList) {
+                //                    var pos = layer._moveList[i];
+                //                    var p = cc.p(pos[0], pos[1]);
+                //                    var distance = cc.pDistanceSQ(prevCC, p);
+                //                    var move = cc.moveTo(distance / (60 * 1000), p);
+                //                    list.push(move);
+                //                    prevCC = p;
+                //                }
+                //
+                //                layer._thunderman.stopAllActions();
+                //                var action = cc.Sequence.create(list);
+                //
+                //                layer._thunderman.runAction(action);
+                //                layer._moveList = [];
             },
 
 
@@ -139,34 +257,6 @@ var HelloWorldLayer = cc.Layer.extend({
                 console.log("end");
                 return;
             }
-        }, this);
-        return true;
-    },
-
-    onUpdate: function(delta) {
-	    var pos = this._draw.convertToWorldSpace(cc.p(0, 0));
-        this._streak.x = pos.x + 32 / 2 - 6 * 2;
-        this._streak.y = pos.y + 32 / 2 - 6 * 2;
-        //this._streak.setPosition(this._thunderman.x + TILE_SIZE / 2, this._thunderman.y + TILE_SIZE / 2);
-
-        for (var i in this._sprites) {
-            var redman = this._sprites[i];
-
-            if (cc.rectOverlapsRect(this._thunderman.getBoundingBox(), redman.getBoundingBox())) {
-                redman.runAction(cc.blink(1, 10));
-            }
-        }
-    },
-
-});
-
-var HelloWorldScene = cc.Scene.extend({
-    onEnter: function() {
-        this._super();
-        var layer = new HelloWorldLayer();
-        this.addChild(layer);
-
-
-
+        }, layer);
     }
 });
